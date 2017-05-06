a61de34d2297f8816c9564c524ead138fa6a04f5
Jeffrey <jeffrey.magmachu@gmail.com>
2017-05-06 15:58:33 -0400

    temp commit because computer is running out of battery

--- src/Azmata.java
+++ src/Azmata.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class Azmata {
     private static final int BLOCK_SIZE = 32;
@@ -15,7 +13,7 @@ public class Azmata {
     public static void main(String[] args) {
         frame = new JFrame();
         panel = new JPanel();
-        frame.getContentPane().setSize(BLOCK_SIZE * SCALE_X * SCALE, BLOCK_SIZE * SCALE_Y * SCALE);
+        frame.getContentPane().setPreferredSize(new Dimension(BLOCK_SIZE * SCALE_X * SCALE, BLOCK_SIZE * SCALE_Y * SCALE));
         frame.add(panel);
         frame.setBackground(Color.WHITE);
         frame.pack();
@@ -23,13 +21,8 @@ public class Azmata {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setVisible(true);
+        g = panel.getGraphics();
         drawLogo();
-        panel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                System.out.println("lol");
-            }
-        });
     }
 
     private static void sleep(long time) {
@@ -41,7 +34,6 @@ public class Azmata {
     }
 
     private static void drawLogo() {
-        g = panel.getGraphics();
         for (int i = 0; i < 100; i++) {
             g.drawRect(100, 100, i, i);
             sleep(10);
