9e06c101534082a5d26d4b97afbda81817571ddc
Jeffrey <jeffrey.magmachu@gmail.com>
2017-05-09 17:26:46 -0400

    added style guide

--- src/Menu/GameMenu.java
+++ src/Menu/GameMenu.java
@@ -1,8 +1,11 @@
 package Menu;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameMenu extends JPanel {
+    Image background_image;
+
     public GameMenu() {
         super();
     }
