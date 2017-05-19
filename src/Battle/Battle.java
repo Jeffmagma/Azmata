f73def4ed73b01b1f23d132adafa70466819c06d
Jeffrey <jeffrey.magmachu@gmail.com>
2017-05-18 22:00:57 -0400

    fixed compile error

--- src/Battle/Battle.java
+++ src/Battle/Battle.java
@@ -1,91 +1,86 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.FontMetrics;
-import java.awt.Dimension;
-import java.awt.event.ActionListener;
+package Battle;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import javax.swing.JPanel;
-import javax.swing.JFrame;
-import javax.swing.Timer;
-import javax.swing.SwingUtilities;
+import java.awt.event.ActionListener;
 import java.util.HashSet;
+
 //import util.Text;
 //How do you import stuff
 //Improves compile speed, will be compressed later TODO
-public class Battle extends JPanel{
-	HashSet<Tile> tiles = new HashSet<Tile>();
-	Timer t;
-	long tickCount;
-	int speed;
+public class Battle extends JPanel {
+    HashSet<Tile> tiles = new HashSet<Tile>();
+    Timer t;
+    long tickCount;
+    int speed;
 
-	ActionListener tick = new ActionListener(){
-		public void actionPerformed(ActionEvent e){
-			++tickCount;
+    ActionListener tick = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            ++tickCount;
 
-			for(Tile tile : tiles){
-				if(tickCount % speed == 0){
-					tile.moveX((int) (tickCount / 50 + (int) tile.hashCode()) % 3 - 2);
-					tile.moveY((int) (tickCount / 50 + (int) tile.hashCode() - 1) % 3 - 2);
-				}
-			}
+            for (Tile tile : tiles) {
+                if (tickCount % speed == 0) {
+                    tile.moveX((int) (tickCount / 50 + tile.hashCode()) % 3 - 2);
+                    tile.moveY((int) (tickCount / 50 + tile.hashCode() - 1) % 3 - 2);
+                }
+            }
 
-			repaint();
-			revalidate();
-		}
-	};
+            repaint();
+            revalidate();
+        }
+    };
 
-	protected void paintComponent(Graphics graphics){
-		Graphics2D g = (Graphics2D) graphics;
-		int x, y, size;
-		String letter;
+    public Battle(int speed) {
+        this.speed = speed;
+        tickCount = 0;
 
-		g.setColor(Color.white);
-		g.fillRect(0, 0, 1024, 576);
+        t = new Timer(20, tick);
+        t.start();
 
-		for(Tile tile : tiles){
-			x = tile.getX();
-			y = tile.getY();
-			size = tile.getSize();
-			letter = "" + tile.getLetter();
+        tiles.add(new Tile('A', 100, 100, 50, Color.red));
+        tiles.add(new Tile('X', 150, 150, 50, Color.green));
+        tiles.add(new Tile('R', 300, 300, 100, Color.blue));
 
-			g.setColor(tile.getColor());
-			g.fillOval(x, y, size, size);
-			
-			g.setFont(new Font("Arial", 0, size));
-			g.setColor(Color.black);
+        setMinimumSize(new Dimension(1024, 576));
+    }
 
-			FontMetrics metrics = g.getFontMetrics(g.getFont());
-			int cx = x - metrics.stringWidth(letter) / 2 + size / 2;
-			int cy = y + (metrics.getAscent() - metrics.getDescent()) / 2 + size / 2;
-			g.drawString(letter, cx, cy);
-		}
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame f = new JFrame();
+                f.setSize(1024, 576);
+                f.setDefaultCloseOperation(2);
+                f.add(new Battle(10));
+                f.setVisible(true);
+            }
+        });
+    }
 
-	public Battle(int speed){
-		this.speed = speed;
-		tickCount = 0;
+    protected void paintComponent(Graphics graphics) {
+        Graphics2D g = (Graphics2D) graphics;
+        int x, y, size;
+        String letter;
 
-		t = new Timer(20, tick);
-		t.start();
+        g.setColor(Color.white);
+        g.fillRect(0, 0, 1024, 576);
 
-		tiles.add(new Tile('A', 100, 100, 50, Color.red));
-		tiles.add(new Tile('X', 150, 150, 50, Color.green));
-		tiles.add(new Tile('R', 300, 300, 100, Color.blue));
+        for (Tile tile : tiles) {
+            x = tile.getX();
+            y = tile.getY();
+            size = tile.getSize();
+            letter = "" + tile.getLetter();
 
-		setMinimumSize(new Dimension(1024, 576));
-	}
+            g.setColor(tile.getColor());
+            g.fillOval(x, y, size, size);
 
-	public static void main(String[] args){
-		SwingUtilities.invokeLater(new Runnable(){
-			public void run(){
-				JFrame f = new JFrame();
-				f.setSize(1024, 576);
-				f.setDefaultCloseOperation(2);
-				f.add(new Battle(10));
-				f.setVisible(true);
-			}
-		});
-	}
+            g.setFont(new Font("Arial", 0, size));
+            g.setColor(Color.black);
+
+            FontMetrics metrics = g.getFontMetrics(g.getFont());
+            int cx = x - metrics.stringWidth(letter) / 2 + size / 2;
+            int cy = y + (metrics.getAscent() - metrics.getDescent()) / 2 + size / 2;
+            g.drawString(letter, cx, cy);
+        }
+    }
 }
--- src/Battle/Tile.java
+++ src/Battle/Tile.java
@@ -1,68 +1,70 @@
-import java.awt.Color;
+package Battle;
 
-public class Tile{
-	private char letter;
-	private int x;
-	private int y;
-	private int size;
-	private Color color;
-	
-	public Tile(char l, int initX, int initY, int initSize, Color c){
-		letter = l;
-		x = initX;
-		y = initY;
-		size = initSize;
-		color = c;
-	}
+import java.awt.*;
 
-	public void moveX(int dx){
-		x = (x + dx) % 1024;
-	}
+public class Tile {
+    private char letter;
+    private int x;
+    private int y;
+    private int size;
+    private Color color;
 
-	public void moveY(int dy){
-		y = (y + dy) % 576;
-	}
+    public Tile(char l, int initX, int initY, int initSize, Color c) {
+        letter = l;
+        x = initX;
+        y = initY;
+        size = initSize;
+        color = c;
+    }
 
-	public void changeSize(int ds){
-		size += ds;
-	}
+    public static void main(String[] args) {
+        Tile t = new Tile('A', 100, 200, 50, Color.blue);
+        Tile u = new Tile('X', 69, 420, 69, Color.red);
+        System.out.println(t.hashCode());
+        System.out.println(u.hashCode());
+    }
 
-	public void setX(int x){
-		this.x = x;
-	}
+    public void moveX(int dx) {
+        x = (x + dx) % 1024;
+    }
 
-	public void setY(int y){
-		this.y = y;
-	}
+    public void moveY(int dy) {
+        y = (y + dy) % 576;
+    }
 
-	public void setSize(int s){
-		size = s;
-	}
+    public void changeSize(int ds) {
+        size += ds;
+    }
 
-	public char getLetter(){
-		return letter;
-	}
+    public char getLetter() {
+        return letter;
+    }
 
-	public int getX(){
-		return x;
-	}
+    public int getX() {
+        return x;
+    }
 
-	public int getY(){
-		return y;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public int getSize(){
-		return size;
-	}
+    public int getY() {
+        return y;
+    }
 
-	public Color getColor(){
-		return color;
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public static void main(String[] args){
-		Tile t = new Tile('A', 100, 200, 50, Color.blue);
-		Tile u = new Tile('X', 69, 420, 69, Color.red);
-		System.out.println(t.hashCode());
-		System.out.println(u.hashCode());
-	}
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int s) {
+        size = s;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
