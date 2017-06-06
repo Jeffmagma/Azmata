8fd93dcc935a44e5373ec849d69911d76b97e502
Jeffmagma <jeffrey.magmachu@gmail.com>
2017-06-06 06:19:50 -0400

    high scores

---
+++ src/Menu/HighScores.java
@@ -0,0 +1,62 @@
+package Menu;
+
+import Main.Azmata;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A class of the options inside the options menu
+ *
+ * @author Jeffrey Gao
+ */
+public class HighScores extends JPanel {
+    /** The scores of all the players */
+    private Map<String, Integer> scores;
+    /** When the user wants to return back to the main menu */
+    private volatile boolean quit;
+
+    /**
+     * TODO: actual stuff
+     */
+    public HighScores() {
+        Azmata.debug("HighScores Constructed");
+        quit = false;
+        getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("ESCAPE"), "quit");
+        getActionMap().put("quit", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quit = true;
+            }
+        });
+        scores = new HashMap<>();
+        // TODO read from save
+        scores.put("lol", 1);
+        scores.put("kek", 2);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        int index = 0;
+        for (Map.Entry score : scores.entrySet()) {
+            String player = (String) score.getKey();
+            int sc = (Integer) score.getValue();
+            // TODO draw the scores
+            g.drawString(player, 20, 100 + index * 30);
+            g.drawString(String.valueOf(sc), 500, 100 + index * 30);
+            index++;
+        }
+    }
+
+    /**
+     * Show the options, returning when the user decides to quit
+     */
+    public void show() {
+        revalidate();
+        repaint();
+        while (!quit) ;
+    }
+}
