263deb51c8d772c7eb825027107e43512a10c97c
Jeffmagma <jeffrey.magmachu@gmail.com>
2017-06-06 05:35:58 -0400

    npcs have locations, some new sprites

--- src/Game/GameState.java
+++ src/Game/GameState.java
@@ -16,20 +16,20 @@ public class GameState implements Serializable {
     final Point player_pos;
     /** The list of NPCs that are currently present in the game */
     public Set<NPC> npc_list;
-    /** If the user is currently inside a game world */
-    boolean in_game;
-    /** Which worlds the player has beaten */
-    boolean[] beaten_worlds;
     /** The name of the map, so that it can be retrieved later */
     public String map_name;
-    /** The current map */
-    private transient GameMap current_map;
     /** The index of the next question */
     public int question;
     /** The amount of health the player has */
     public double health;
     /** The player's score */
     public long score;
+    /** If the user is currently inside a game world */
+    boolean in_game;
+    /** Which worlds the player has beaten */
+    boolean[] beaten_worlds;
+    /** The current map */
+    private transient GameMap current_map;
 
     /**
      * Constructs a game state with the player at a specified position
@@ -40,11 +40,34 @@ public class GameState implements Serializable {
         map_name = world.getMapName();
         player_pos = world.getStartingPoint();
         npc_list = new HashSet<>();
-        beaten_worlds = new boolean[] {false, false, false};
+        beaten_worlds = new boolean[]{false, false, false};
         in_game = true;
         health = 100.0;
         score = 0;
         question = 0;
+        for (Point p : world.npcPoints()) {
+            String sheet, battler;
+            switch ((int) (Math.random() * 3)) {
+                case 0:
+                    sheet = "Orc.png";
+                    battler = "Orc.png";
+                    break;
+                case 1:
+                    sheet = "ghost.png";
+                    battler = "Ghost.png";
+                    break;
+                case 2:
+                default:
+                    sheet = "zombie_1.png";
+                    battler = "Zombie.png";
+            }
+            npc_list.add(new NPC(p, new SpriteSheet(sheet, "eric.png"), battler) {
+                @Override
+                public void onTalk() {
+                    battling = true;
+                }
+            });
+        }
     }
 
     /**
